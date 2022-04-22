3fd43e2854a8fc963014bf266b1288d1924d64b0
And1 <92379947+And1x@users.noreply.github.com>
2022-04-22 20:05:19 +0200

    minor changes - replaced if statements with switch - before refactoring some old stuff

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ Set the time with flag at end of command - in Minutes
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"pomodoro/helpers"
 	"pomodoro/sound"
 	"strconv"
@@ -17,15 +19,16 @@ import (
 
 func main() {
 
-	//setFrames := createFrames("*", 60)
+	setFrames := createFrames("█", 60) // 🟩 ⬛ ▀▄ █
+	//fmt.Println(setFrames)
 
 	timeframe := setTimer() * 60 //*60 hence we calculate in seconds - timeframe gets time in minutes
 	pause := timeframe / 5
 
-	tik := time.NewTicker(50 * time.Millisecond)
+	tik := time.NewTicker(1 * time.Second)
 
-	// i is our controll variable which represents the seconds elapsed
-	i := 0
+	// i is our controll variable which represents the seconds elapsed, j is the animation counter
+	i, j := 0, 0
 
 	// startTime := fmt.Sprintf("Start: %s", time.Now())
 	startTime := time.Now()
@@ -36,38 +39,73 @@ func main() {
 	// clear screen and jump at start of screen
 	fmt.Print("\033[H\033[2J")
 
+timeLoop:
 	for range tik.C {
 		i++
+		j++
 
-		fmt.Printf("%s", "#")
-		if i%5 == 0 {
+		switch {
+		case i%60 == 0:
+			fmt.Print("\033[H\033[2J") // clear whole screen
+			j = 0
+		case i >= timeframe && phaseEnd == 0:
+			phaseEnd++
+			//## tik.Stop()
 
-			fmt.Printf("\x1b[2J") // clear whole screen
-			fmt.Print("\033[H")   // jump at the start of the screen
-			//fmt.Printf("%vs: ", i)
-			fmt.Printf("%dm: ", i/60)
-		}
-
-		if i >= timeframe && phaseEnd == 0 {
-			phaseEnd += 1
-			//tik.Stop()
-			fmt.Println("xx", phaseEnd)
-			fmt.Println("start", startTime)
+			// fmt.Println("start", startTime)
 			endTime := time.Now()
-			fmt.Println("Ende ", endTime)
+			// fmt.Println("Ende ", endTime)
 			sound.PlaySound() // play sound at the end
 			helpers.Logger(startTime, endTime)
-			//break
-		} else if i >= (timeframe + pause) {
+
+			fmt.Printf("do you want a break for %dsec? y/n \n", pause)
+			if makeBreak() { // check if user wants a break - if yes continue to tik else stop here
+				continue
+			} else {
+				tik.Stop()
+				break timeLoop
+			}
+			//## break timeLoop
+		case i >= timeframe+pause && phaseEnd == 1:
 			tik.Stop()
-			fmt.Println("")
-			fmt.Println("startBREAK", startTime)
-			endTime := time.Now()
-			fmt.Println("EndeBREAK ", endTime)
 			sound.PlaySound() // play sound at the end
-			// helpers.Logger(startTime, endTime)
-			break
+			break timeLoop
+
+		default:
+			fmt.Print("\033[H\033[2J")
+			fmt.Printf("\033[39m%v\n\033[31m%d\n\n", setFrames[j], i)
+
 		}
+		/*
+			fmt.Printf("%s", "#")
+			if i%5 == 0 {
+
+				fmt.Printf("\x1b[2J") // clear whole screen
+				fmt.Print("\033[H")   // jump at the start of the screen
+				//fmt.Printf("%vs: ", i)
+				fmt.Printf("%dm: ", i/60)
+			}
+
+			if i >= timeframe && phaseEnd == 0 {
+				phaseEnd += 1
+				//tik.Stop()
+				fmt.Println("xx", phaseEnd)
+				fmt.Println("start", startTime)
+				endTime := time.Now()
+				fmt.Println("Ende ", endTime)
+				sound.PlaySound() // play sound at the end
+				helpers.Logger(startTime, endTime)
+				//break
+			} else if i >= (timeframe + pause) {
+				tik.Stop()
+				fmt.Println("")
+				fmt.Println("startBREAK", startTime)
+				endTime := time.Now()
+				fmt.Println("EndeBREAK ", endTime)
+				sound.PlaySound() // play sound at the end
+				// helpers.Logger(startTime, endTime)
+				break
+			}*/
 	}
 
 }
@@ -83,12 +121,29 @@ func setTimer() (timeframe int) {
 	return
 }
 
+// createFrames creates the Frames to loop trough to get an animation... Probably unnecessarry - could be improved later on.
 func createFrames(symbol string, length int) []string {
 
 	animationFrames := []string{}
 	for i := 0; i <= length; i++ {
-		step := strings.Repeat("", i)
+		step := strings.Repeat(symbol, i)
 		animationFrames = append(animationFrames, step)
 	}
 	return animationFrames
+}
+
+// makeBreak ask user to make a break if he types y/Y then true -- else: false
+func makeBreak() bool {
+	reader := bufio.NewReader(os.Stdin)
+	char, _, err := reader.ReadRune()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if char == 'y' || char == 'Y' {
+		return true
+	} else if char == 'n' || char == 'N' {
+		return false
+	}
+	return false
 }
